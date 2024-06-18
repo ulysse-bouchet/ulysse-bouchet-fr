3810fb1118cb7771422f8db81f58c1863f922ffc
ulysse-bonneau <ulysse.bonneau@proton.me>
2024-06-18 11:10:54 +0200

    [0.0.1] Initialise project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {}
-	},
-	plugins: []
-};
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        'text': '#0e0c0d',
+        'background': '#f7f5f6',
+        'primary': '#FF90BC',
+        'secondary': '#FFC0D9',
+        'accent': '#ba8a9e',
+      },
+      fontSize: {
+        sm: '0.750rem',
+        base: '1rem',
+        xl: '1.333rem',
+        '2xl': '1.777rem',
+        '3xl': '2.369rem',
+        '4xl': '3.158rem',
+        '5xl': '4.210rem',
+      },
+      fontFamily: {
+        heading: 'Inter',
+        body: 'Inter',
+      },
+      fontWeight: {
+        normal: '400',
+        bold: '700',
+      },
+    },
+  },
+  plugins: [],
+}
+
