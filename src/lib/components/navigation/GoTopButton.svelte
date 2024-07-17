9963096e8da4e8f0f3d0494ed849f3c50f366049
Ulysse B. <ulysse.bonneau@proton.me>
2024-07-17 11:07:04 +0200

    [2.7.1] Added go to top button

---
+++ src/lib/components/navigation/GoTopButton.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	const goToTop = () => {
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+	};
+
+	const toggleButtonVisibility = () => {
+		const goTopButton = document.getElementById('goTopButton');
+		if (!goTopButton) return;
+
+		if (window.scrollY < 50)
+			goTopButton.style.visibility = 'hidden';
+		else
+			goTopButton.style.visibility = 'visible';
+	};
+
+	onMount(() => {
+		window.addEventListener('scroll', toggleButtonVisibility);
+		toggleButtonVisibility();
+	});
+</script>
+
+<button id="goTopButton" on:click={goToTop} class="fixed bottom-8 right-4 p-2 bg-primary text-background z-10">
+	<img src="arrow_top.svg" alt="An arrow pointing towards the top of the page" class="h-8" />
+</button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import '../app.css';
 	import NavBar from '$lib/components/navigation/NavBar.svelte';
+	import GoTopButton from '$lib/components/navigation/GoTopButton.svelte';
 </script>
 
 <div class="min-h-screen min-w-screen bg-background font-body text-base text-text">
 	<slot />
 	<NavBar />
+	<GoTopButton />
 </div>
