cfd4b031618a982855b74d29638c7949de7d79c4
Ulysse B. <ulysse.bonneau@proton.me>
2024-08-29 16:55:27 +0200

    [2.13.1] Update go to top button design

--- src/lib/components/navigation/GoTopButton.svelte
+++ src/lib/components/navigation/GoTopButton.svelte
@@ -9,10 +9,15 @@
 		const goTopButton = document.getElementById('goTopButton');
 		if (!goTopButton) return;
 
-		if (window.scrollY < 50)
+		if (window.scrollY < 50) {
 			goTopButton.style.visibility = 'hidden';
-		else
+		} else if (window.scrollY + window.innerHeight >= document.body.scrollHeight) {
+			goTopButton.classList.add('bg-transparent', 'shadow-none', 'text-background');
 			goTopButton.style.visibility = 'visible';
+		} else {
+			goTopButton.classList.remove('bg-transparent', 'shadow-none', 'text-background');
+			goTopButton.style.visibility = 'visible';
+		}
 	};
 
 	onMount(() => {
@@ -21,9 +26,13 @@
 	});
 </script>
 
-<button id="goTopButton" on:click={goToTop} class="fixed bottom-8 right-4 bg-secondary border-2 border-background z-10">
-	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"
-			 class="h-12 w-12 p-2 fill-background border-2 border-text">
-		<path d="M440-160v-487L216-423l-56-57 320-320 320 320-56 57-224-224v487h-80Z" />
-	</svg>
+<button
+	id="goTopButton"
+	on:click={goToTop}
+	class="fixed bg-background opacity-80 shadow rounded-lg px-4 py-2 bottom-3 right-1/2 translate-x-1/2 z-10"
+>
+	<div class="flex items-center text-inherit space-x-2">
+		<span> Revenir en haut </span>
+		<span> ↑ </span>
+	</div>
 </button>
