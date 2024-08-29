28c5a3b439c82c89f4a682459cd148a6562954b3
Ulysse B. <ulysse.bonneau@proton.me>
2024-08-29 16:52:23 +0200

    [2.12.2] Remove .idea settigns folder from git

--- src/lib/sections/Footer.svelte
+++ src/lib/sections/Footer.svelte
@@ -1,6 +1,6 @@
-<div class="py-3 flex flex-col items-center justify-center space-y-1 bg-accent text-background text-center text-sm">
+<div class="py-3 pb-16 flex flex-col items-center justify-center space-y-1 bg-accent text-background text-center text-sm">
 	<span>Site développé par Ulysse Bouchet.</span>
 	<span class="italic text-xs">
-		v2.12.1 - dernière mise à jour : 19/07/2024
+		v2.12.2 - dernière mise à jour : 29/08/2024
 	</span>
 </div>
