663293ded43227e9fc90b951cec994df7c957464
Ulysse B. <ulysse.bonneau@proton.me>
2024-09-12 10:10:27 +0200

    [2.16.4] Upgrade packages

--- src/lib/index.ts
+++
@@ -1,2 +0,0 @@
-
-<div class="sticky bottom-20 h-[2px] w-0 bg-gradient-to-r from-primary via-secondary to-primary" id="scrollbar"></div>// place files you want to import through the `$lib` alias in this folder.
