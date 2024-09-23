b008f720279c3cae5096d93138da59a17e2f053b
Ulysse B. <ulysse.bonneau@proton.me>
2024-09-23 13:47:46 +0200

    [2.17.3] CV is now downloadable

--- src/lib/sections/about/AboutSection.svelte
+++ src/lib/sections/about/AboutSection.svelte
@@ -2,7 +2,10 @@
 	import TriangleCanvas from '$lib/sections/about/TriangleCanvas.svelte';
 </script>
 
-<div id="about" class="relative min-h-screen min-w-screen bg-background rounded-b-3xl shadow-xl z-50">
+<div
+	id="about"
+	class="relative min-h-screen min-w-screen bg-background rounded-b-3xl shadow-xl z-50"
+>
 	<div class="flex flex-col items-center">
 		<img
 			alt="Ulysse Bouchet"
@@ -24,8 +27,9 @@
 			<TriangleCanvas />
 		</div>
 		Bonjour, et bienvenue sur mon site !<br /> <br />
-		Je suis un jeune ingénieur en informatique, spécialisé dans le <span class="text-accent font-semibold">développement web</span>. Fraîchement diplômé
-		de l'Efrei Paris, je suis maintenant à la recherche d'un emploi dans ce domaine qui me passionne.
+		Je suis un jeune ingénieur en informatique, spécialisé dans le
+		<span class="text-accent font-semibold">développement web</span>. Fraîchement diplômé de l'Efrei
+		Paris, je suis maintenant à la recherche d'un emploi dans ce domaine qui me passionne.
 		<br /> <br />
 		J'ai pu acquérir de nombreuses compétences grâce à plusieurs stages, mais surtout à l'issue de ma
 		formation par apprentissage au CEA Paris-Saclay. J'ai également eu l'occasion de rédiger un mémoire
@@ -33,7 +37,11 @@
 		Pour en savoir plus à mon sujet, je vous invite à continuer votre visite en scrollant plus bas !
 	</div>
 	<div class="flex justify-center py-6">
-		<button class="text-center text-primary flex items-center space-x-1">
+		<a
+			href="CV - Version française (2024).pdf"
+			download="CV - Version française (2024).pdf"
+			class="text-center text-primary flex items-center space-x-1"
+		>
 			<span> Télécharger mon CV </span>
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
@@ -46,6 +54,6 @@
 					d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"
 				/>
 			</svg>
-		</button>
+		</a>
 	</div>
 </div>
